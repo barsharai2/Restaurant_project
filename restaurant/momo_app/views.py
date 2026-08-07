624fd70fa127f6b19d5632ae066d44fe4e42ef15
Barsha Rai <raibarsha579@gmail.com>
2026-08-07 13:24:45 +0545

    aos,favicon,form

--- restaurant/momo_app/views.py
+++ restaurant/momo_app/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
 # Create your views here.
 def index(request):
-    return render(request,'momo_app/index.html')
+    if request.method=='POST':
+        name=request.POST['name']
+        number=request.POST['number']
+        email=request.POST['email']
+        message=request.POST['message']
+        Form.objects.create(name=name,email=email,number=number,message=message)
+        return redirect('index')
+    return render(request,'momo_app/index.html')  
 
 def contact(request):
     return render(request,'momo_app/contact.html')
