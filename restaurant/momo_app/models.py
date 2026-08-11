a0b3f0e00e70829e2cd8ced379f2fd3978f636bf
Barsha Rai <raibarsha579@gmail.com>
2026-08-11 13:49:56 +0545

    testemonial dynamic, all item

--- restaurant/momo_app/models.py
+++ restaurant/momo_app/models.py
@@ -12,16 +12,23 @@ class Form(models.Model):
 
 class Category(models.Model):
     title=models.CharField(max_length=200)
+    image=models.ImageField(upload_to='category_images',null=True)
 
     def __str__(self):
         return self.title
 
 class Momo(models.Model):
     name=models.CharField(max_length=200)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name='items',null=True)
     desc=models.TextField()
     image=models.ImageField(upload_to='images')
     price=models.DecimalField(max_digits=8,decimal_places=2)
     is_available=models.BooleanField(default=True)
     create_at=models.DateField(auto_now_add=True)
     update_at=models.DateField(auto_now=True)
+
+class Review(models.Model):
+    name=models.CharField(max_length=150)
+    message=models.TextField()
+    rating=models.PositiveBigIntegerField()
+    order=models.CharField(max_length=250)
--- restaurant/momo_app/views.py
+++ restaurant/momo_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .models import *
+import qrcode
 # Create your views here.
 def index(request):
     category=Category.objects.all()
     cateid=request.GET.get('category')
-    if cateid:
+    if cateid=='all':
+        momo=Momo.objects.filter(is_available=True)
+    elif cateid:
         momo=Momo.objects.filter(is_available=True,category=cateid)
     else:
         momo=Momo.objects.filter(is_available=True)
@@ -31,12 +34,27 @@ def about(request):
     return render(request,'momo_app/about.html')
 
 def menu(request):
-    return render(request,'momo_app/menu.html')
+    category=Category.objects.all()
+    qr=qrcode.make("http://127.0.0.1:8000/menu/")
+    qr.save('momo_app/static/images/qr.png')
+    context={
+        'category':category
+    }
+    return render(request,'momo_app/menu.html',context)
 
 def services(request):
     return render(request,'momo_app/services.html')
 
 def testemonial(request):
-    return render(request,'momo_app/testemonial.html')
+    teste=Momo.objects.all()
+    if request.method == "POST":
+        name=request.POST['name']
+        rating=request.POST['rating']
+        order=request.POST.get('order')
+        message=request.POST['message']
+        Review.objects.create(name=name,rating=rating,order=order,message=message)
+
+    context={'momos':teste}
+    return render(request,'momo_app/testemonial.html',context)
 def term(request):
     return render(request,'momo_app/terms.html')
